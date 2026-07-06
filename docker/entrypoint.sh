c53efd779df1b4b8cf1f9826014dbca191314bdb
Developer <dev@mobile-code-server.com>
2026-07-06 14:30:56 +0700

    Add nginx for React wrapper UI + fix entrypoint process management
    - Install nginx in Dockerfile
    - Copy nginx.conf to serve React app on port 8080 and proxy code-server on 8081
    - Rewrite entrypoint to properly start nginx as coder user, then exec code-server
    - Entrypoint runs as root to manage both processes

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 set -e
 
-# Start code-server in the background
-/usr/bin/entrypoint.sh "$@" &
+# Fix ownership of nginx runtime directories
+chown -R coder:coder /var/cache/nginx /var/lib/nginx /var/log/nginx
+mkdir -p /var/log/nginx /var/lib/nginx /var/cache/nginx
+touch /var/run/nginx.pid
+chown -R coder:coder /var/run/nginx.pid
 
-# Wait a moment for code-server to start
-sleep 5
+# Start nginx as coder user in background
+su -s /bin/bash -c 'nginx -g "daemon on;"' coder
 
-# Keep the container running
-tail -f /dev/null
+# Start code-server as coder user (exec replaces this process)
+exec su -s /bin/bash coder -c 'exec "$0" "$@"' -- code-server "$@"
